a16d5b59a67342f4286afc14f3865479120f327f
Jojorandet <josephine@randet.com>
2025-10-07 17:37:49 +0200

    going home im tired, but my code is clean

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,9 +3,25 @@
 
 int main()
 {
-	FragTrap Fraggy("Alice");
-	Fraggy.attack("Bob");
-	Fraggy.highFiveGuys();
-	Fraggy.beRepaired(10);
+	{
+		FragTrap Fraggy("Fraggy");
+		FragTrap Scrap("Scrap");
+		Fraggy.setAttackDammage(10);
+		Fraggy.attack("Scrap");
+		Scrap.takeDammage(10);
+		std::cout << "Scrap has " << Scrap.getHitPoints() << " hitpoints "
+				<< "and " << Scrap.getEnergyPoints() << " energy points" << '\n';
+		std::cout << "Fragy has " << Fraggy.getHitPoints() << " hitpoints "
+				<< " and " << Fraggy.getEnergyPoints() << " energy points" << '\n';
+		Fraggy.highFiveGuys();
+		Scrap.beRepaired(10);
+		Scrap.attack("Fraggy");
+	}
+	std::cout << '\n';
+	{
+		FragTrap Alice("Alice");
+		std::cout << "Alice has " << Alice.getHitPoints() << " hitpoints and " 
+				<< Alice.getEnergyPoints() << " energypoints." << '\n';
+	}
 	return 0;
 }
